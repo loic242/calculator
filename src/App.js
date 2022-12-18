e8c1c446e4191b72a9b60d157569fa27511a6d56
loic242 <bmavah@gmail.com>
2022-12-18 15:10:36 +0200

    css done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 
-import './App.css';
 
 function App() {
+  const creatDigits =()=>{
+    const digits =[]
+    for(let i=1; i<10; i++){
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
   return (
     <div className="App">
-   <div className="calculatore">
-    <div className="display">
-      <span>(0)</span>0
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>0
+        </div>
+     
+      <div className="operator">
+         <button>/</button>
+         <button>*</button>
+         <button>+</button>
+         <button>-</button>
+         <button>Del</button>
+     </div>
+     <div className="digitals">
+       {creatDigits()}
+       <button>0</button>
+       <button>.</button>
+       <button>=</button>
+     </div>
     </div>
-   </div>
-   <div className="operator">
-    <buttom>/</buttom>
-    <buttom>*</buttom>
-    <buttom>+</buttom>
-    <buttom>-</buttom>
-    <button>Del</button>
-   </div>
-   <div className="digitals">
-    <button>0</button>
-    <button>.</button>
-    <button>=</button>
-   </div>
-    </div>
+  </div>
   );
 }
 
