b9ba0dcfbbfab2f428a8bb2a9fe4e3f84d3e1c37
loic242 <bmavah@gmail.com>
2022-12-18 12:38:14 +0200

    add button to the calculator

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ function App() {
     </div>
    </div>
    <div className="operator">
-    
+    <buttom>/</buttom>
+    <buttom>*</buttom>
+    <buttom>+</buttom>
+    <buttom>-</buttom>
+    <button>Del</button>
+   </div>
+   <div className="digitals">
+    <button>0</button>
+    <button>.</button>
+    <button>=</button>
    </div>
     </div>
   );
